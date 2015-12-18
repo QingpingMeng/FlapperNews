5a2130c32fe0d89bd28b6d10664e097db5454c08
QingpingMeng <missing1989@gmail.com>
2015-12-17 21:41:52 -0800

    Done

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,6 +1,26 @@
-var app = angular.module('flapperNews',['ui.router']);
+var app = angular.module('flapperNews',['ui.router']).run(['$rootScope',function($rootScope){
+	$rootScope.$on('$stateChangeStart',function(event, toState, toParams, fromState, fromParams){
+		console.log('$stateChangeStart to '+toState.to+'- fired when the transition begins. toState,toParams : \n',toState, toParams);
+	});
+	$rootScope.$on('$stateChangeError',function(event, toState, toParams, fromState, fromParams, error){
+		console.log('$stateChangeError - fired when an error occurs during transition.');
+		console.log(arguments);
+	});
+	$rootScope.$on('$stateChangeSuccess',function(event, toState, toParams, fromState, fromParams){
+		console.log('$stateChangeSuccess to '+toState.name+'- fired once the state transition is complete.');
+	});
+	$rootScope.$on('$viewContentLoaded',function(event){
+		console.log('$viewContentLoaded - fired after dom rendered',event);
 
-app.factory('posts',["$http",function($http){
+	});
+	$rootScope.$on('$stateNotFound',function(event, unfoundState, fromState, fromParams){
+		console.log('$stateNotFound '+unfoundState.to+'  - fired when a state cannot be found by its name.');
+		console.log(unfoundState, fromState, fromParams);
+
+	});
+}]);
+
+app.factory('posts',["$http",'auth',function($http,auth){
 	//service body
 	var o = {
 		posts:[]
@@ -13,13 +33,17 @@ app.factory('posts',["$http",function($http){
 	};
 
 	o.create=function(post){
-		return $http.post("/posts",post).success(function(data){
+		return $http.post("/posts",post,{
+			headers:{Authorization: 'Bearer '+auth.getToken()}
+		}).success(function(data){
 			o.posts.push(data);
 		});
 	};
 
 	o.upvote = function(post){
-		return $http.put('/posts/'+post._id+'/upvote')
+		return $http.put('/posts/'+post._id+'/upvote',null,{
+			headers:{Authorization: 'Bearer '+auth.getToken()}
+		})
 		.success(function(data){
 			post.upvotes +=1;
 		});
@@ -32,16 +56,79 @@ app.factory('posts',["$http",function($http){
 	};
 
 	o.addComment = function(id,comment){
-		return $http.post('/posts/'+id + '/comments',comment);
-	}
+		return $http.post('/posts/'+id + '/comments',comment,{
+			headers:{Authorization: 'Bearer '+auth.getToken()}
+		});
+	};
+
+	o.upvoteComment = function(post,comment){
+		return $http.put('/posts/'+post._id+'/comments/'+comment._id+ '/upvote',null,{
+			headers:{Authorization: 'Bearer '+auth.getToken()}
+		}).
+			success(function(data){
+				comment.upvotes +=1;
+			});
+	};
 	return o;
+}]).factory('auth',['$http','$window',function($http,$window){
+	var auth = {};
+
+	auth.saveToken = function(token){
+		$window.localStorage['flapper-news-token'] = token;
+	};
+
+	auth.getToken = function(token){
+		return $window.localStorage['flapper-news-token'];
+	};
+
+	auth.isLoggedIn = function(){
+		var token = auth.getToken();
+
+		if(token){
+			var payload = JSON.parse($window.atob(token.split('.')[1]));
+			return payload.exp > Date.now() / 1000;
+		}else{
+			return false;
+		}
+	};
+
+	auth.currentUser = function(){
+		var token = auth.getToken();
+
+		if(token){
+			var payload = JSON.parse($window.atob(token.split('.')[1]));
+			return payload.username;
+		}else{
+			return null;
+		}
+	};
+
+	auth.register = function(user){
+		return $http.post('/register',user).success(function(data){
+			auth.saveToken(data.token);
+		});
+	};
+
+	auth.logIn = function(user){
+		return $http.post('/login',user).success(function(data){
+			auth.saveToken(data.token);
+		});
+	};
+
+	auth.logOut = function(){
+		$window.localStorage.removeItem('flapper-news-token');
+	};
+
+	return auth;
 }]);
 
 app.controller('MainCtrl',[
 	'$scope',
 	'posts',
-	function($scope,posts){
+	'auth',
+	function($scope,posts,auth){
 		$scope.posts=posts.posts;
+		$scope.isLoggedIn = auth.isLoggedIn;
 		$scope.test = 'Hello world!';
 		$scope.addPost = function(){
 			if(!$scope.title || $scope.title===''){return;}
@@ -58,15 +145,42 @@ app.controller('MainCtrl',[
 	}
 ]);
 
+app.controller('AuthCtrl',[
+	'$scope',
+	'$state',
+	'auth',
+	function($scope, $state, auth){
+		$scope.user = {};
+
+		$scope.register = function(){
+			auth.register($scope.user).error(function(error){
+				$scope.error = error;
+			}).then(function(){
+				$state.go('home');
+			});
+		};
+
+		$scope.logIn = function(){
+			auth.logIn($scope.user).error(function(error){
+				$scope.error = error;
+			}).then(function(){
+				$state.go('home');
+			});
+		};
+	}
+]);
+
 app.controller('PostsCtrl',[
 	'$scope',
 	'posts',
 	'post',
-	function($scope,posts,post){
+	'auth',
+	function($scope,posts,post,auth){
 		$scope.post=post;
+		$scope.isLoggedIn = auth.isLoggedIn;
 		$scope.addComment = function(){
 			if($scope.body===''){return;}
-			$posts.addComment(post._id,{
+			posts.addComment(post._id,{
 				body:$scope.body,
 				author:'user',
 			}).success(function(comment){
@@ -74,6 +188,20 @@ app.controller('PostsCtrl',[
 			});
 			$scope.body = '';
 		};
+
+		$scope.incrementUpvotes = function(comment){
+			posts.upvoteComment(post,comment);
+		};
+	}
+]);
+
+app.controller('NavCtrl',[
+	'$scope',
+	'auth',
+	function($scope,auth){
+		$scope.isLoggedIn = auth.isLoggedIn;
+		$scope.currentUser = auth.currentUser;
+		$scope.logOut = auth.logOut;
 	}
 ]);
 
@@ -88,20 +216,38 @@ app.config([
 			controller:'MainCtrl',
 			resolve:{
 				postPromise:['posts',function(posts){
+					console.log("Posts.getAll");
 					return posts.getAll();
 				}]
 			}
-		});
-
-		$stateProvider.state('posts',{
+		}).state('posts',{
 			url: '/posts/{id}',
 			templateUrl: '/posts.html',
 			controller: 'PostsCtrl',
 			resolve:{
 				post:['$stateParams','posts',function($stateParams,posts){
+					console.log("Posts.get");
 					return posts.get($stateParams.id);
 				}]
 			}
+		}).state('login',{
+			url:'/login',
+			templateUrl: '/login.html',
+			controller:'AuthCtrl',
+			onEnter:['$state','auth',function($state,auth){
+				if(auth.isLoggedIn()){
+					$state.go('home');
+				}
+			}]
+		}).state('register',{
+			url:'/register',
+			templateUrl:'/register.html',
+			controller:'AuthCtrl',
+			onEnter:['$state','auth',function($state,auth){
+				if(auth.isLoggedIn()){
+					$state.go('home');
+				}
+			}]
 		});
 
 		$urlRouterProvider.otherwise('home');
